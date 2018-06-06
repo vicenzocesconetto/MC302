30cb0163ba2bf1b7be82b5582de81add19cb6af7
vicenzo-cesconetto <vicenzoccesconetto@gmail.com>
2018-06-05 22:05:24 -0300

    answer Part 1 questions

--- lab08/src/Main.java
+++ lab08/src/Main.java
@@ -1,3 +1,53 @@
+/*
+
+PARTE I
+
+1. É possível uma interface herdar de uma classe abstrata? É possível uma classe abstrata implementar
+uma interface?
+
+Uma interface soh pode herdar de uma interface. Sim.
+
+
+2. Quais as principais diferenças entre classes abstratas e interfaces?
+
+Classes abstratas podem ter metodos nao abstratos, interfaces nao. Todos os membros na interface eh publico, enquanto que
+na classe abstrata podem haver membros privados.
+
+
+3. O que acontece se uma classe implementar 2 interfaces diferentes, mas que tem um método com o
+mesmo nome e assinatura?
+
+Implementando esse tal metodo, tudo fica certo e sao implementadas duas interfaces ao mesmo tempo.
+
+
+4. No caso geral, não é possível implementar métodos em interfaces. Mas a partir do Java 8, é possível
+um de dois modificadores aos métodos de uma interface para que seja possível implementá-los.
+Quais são esses modificadores?
+
+static e default
+
+
+PARTE II
+
+1. Qual a diferença na leitura através de um objeto BufferedInputStream para um InputStream
+(incluindo suas subclasses)?
+
+
+2. A classe DataOutputStream tem alguns métodos para escrita, com as assinaturas descritas abaixo.
+Esses métodos dão throw em exceções do tipo IOException. Por que esse tipo de exceção é
+jogada? Em que contextos faz sentido jogar uma exceção do tipo IOException?
+public final void writeInt(int i) throws IOException;
+public final void writeFloat(float f) throws IOException;
+public final void writeByte(int b) throws IOException;
+public final void writeChar(int i) throws IOException;
+...;
+
+
+3. Explique a utilidade da interface Serializable, como ela pode ser usada com arquivos e cite
+duas exceções que métodos dessa interface jogam.
+
+*/
+
 import java.util.ArrayList;
 
 public class Main {
